117670493aa30d42c52821423dd96405d686806f
bifot <bifot@bifot.ru>
2017-10-21 17:26:13 +0300

    refactor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
   Botact: require('./lib/core'),
   api: require('./lib/api'),
-  getLastMessage: require('./lib/modules/getlastmessage')
+  getLastMessage: require('./lib/utils/getLastMessage')
 }
